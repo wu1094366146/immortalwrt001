a3ab6615a1ed94003f432eb2ef01612e6dd68cba
wu1094366146 <61652427+wu1094366146@users.noreply.github.com>
2023-06-05 15:10:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
+#echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.defaul
